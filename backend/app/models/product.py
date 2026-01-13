5236c57cb90ba874db45ed9d36c1bf2866158fd5
Shqumel <shumel_kv_23@mf.grsu.by>
2026-01-13 18:57:43 +0300

    add models for category and product

---
+++ backend/app/models/product.py
@@ -0,0 +1,19 @@
+from sqlalchemy import Column, Integer, String, Text, Float, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
+from ..database import Base
+from datetime import datetime
+
+class Product(Base):
+    __tablename__ = 'products'
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False, index = True)
+    Description = Column(Text)
+    price = Column(Float, nullable=False)
+    category_id = Column(Integer, ForeignKey("categories.id"), nullable=False)
+    image_url = Column(String)
+    created_at = Column(DateTime, default=datetime.utcnow)
+
+    category = relationship("Category", back_populates="products")
+
+    def __repr__(self):
+        return f"<Product(id = {self.id}, name = {self.name}, price = {self.price}')>"
